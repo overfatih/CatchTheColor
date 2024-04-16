91b4c3afb0f720ca04484ae0157e2ba6252f162f
Fatih ÇÖREKÇİ <overfatih@gmail.com>
2024-04-16 13:30:41 +0300

    added total puan, level, auto start level, timeout

--- app/src/main/java/com/profplay/catchthecorrectcolor/MainActivity.kt
+++ app/src/main/java/com/profplay/catchthecorrectcolor/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.profplay.catchthecorrectcolor
 
 import android.graphics.Color
-import android.media.Image
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,9 +8,10 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
-import androidx.core.view.get
+import androidx.core.view.isVisible
 import com.profplay.catchthecorrectcolor.databinding.ActivityMainBinding
 import kotlinx.coroutines.Runnable
 
@@ -23,23 +23,13 @@ class MainActivity : AppCompatActivity() {
     var randomNumber: Int = 0
     private lateinit var circleList: List<Circle>
 
-    var number: Double =0.0
-    var bestScore : Double? = null
-    var runnable : Runnable = Runnable {  }
-    var handler : Handler = Handler(Looper.getMainLooper())
-    /*
-    doğru ve yanlış için iki circle sınıfı oluştur
-    hepsini başta gizle
-    Bir tane doğru renk ata diğerlerini rastgele  yanlış sınıfından ata
-    hedef rengin konumu değişsin
-    sınıf içine funtion larını yaz
-        doğru tıklandığında yeni soru gelsin yanlış tıklandığında game over olsun.
-    ----
-    ok* skorView oluştur
-    en hızlı yakalamayı skora işle
-    renk ve konuma göre hızlı yakalamalar arasında bir fark var mı? sorusu için verileri depola
-    daha sonra alınan demokratif özelliklere göre karşılaştırma yapılabilir. :)
-    */
+    var number :Double =0.0
+    var bestScore :Double? = null
+    var level :Int = 1
+    var puan :Double = 0.0
+    var runnable :Runnable = Runnable {  }
+    var handler :Handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -55,91 +45,99 @@ class MainActivity : AppCompatActivity() {
         val scale = this.resources.displayMetrics.density
         for(item in myColors){
             circleView = ImageView(this)
-            binding.cicleLinearLayout.addView(circleView)
-            //circleView!!.id = "@+id/circleView ${item.key}" as Int
+            binding.circleLinearLayout.addView(circleView)
             circleView?.let {
                 it.id = item.key
                 it.layoutParams.height=  (94.toFloat() * scale + 0.5f).toInt()
                 it.layoutParams.width =  (97.toFloat() * scale + 0.5f).toInt()
                 it.setImageResource(R.drawable.daire)
                 it.setColorFilter(item.value.get(0).toString().toInt())
-                it.setOnClickListener {view ->
-                    betouched(view) }
                 circleList = listOf(Circle(this,0))
             }
 
         }
-        binding.circleImage.visibility = View.GONE
+        binding.levelTextView.text = "Level: ${level.toString()}"
+
 
     }
 
     fun startAction(view:View){
         randomNumber = (1..4).random()
-
         binding.colorTextView.text =  myColors[randomNumber]!!.get(1).toString()
-        //circleList.get(randomNumber).setOnClickListener { catchedCircle(it) }
-        myColors.keys.shuffled()
-
-        for (iv in binding.cicleLinearLayout.children){
-            var imageView: ImageView = iv as ImageView
-            imageView.setColorFilter(myColors[2]!!.get(0).toString().toInt())
-
+        var shuffledColor = arrayOf(1,2,3,4)
+        shuffledColor.shuffle()
+        for (iv in binding.circleLinearLayout.children){
+            var imageView = iv as ImageView
+            imageView.isEnabled=true
+            imageView.setColorFilter(myColors[shuffledColor[iv.id -1]]!!.get(0).toString().toInt())
+            if(shuffledColor[iv.id -1]==randomNumber){
+                imageView.setOnClickListener {view -> catchedCircle(view) }
+            }else {
+                imageView.setOnClickListener { view -> betouched(view) }
+            }
         }
-        //circleList.forEach {
-        //    it.getIsCorret().toString()
-        //}
 
-        //binding.circleImage.setColorFilter(myColors[randomNumber]!!.get(0).toString().toInt())
         binding.startButton.isEnabled= false
-        //binding.circleImage.visibility = View.VISIBLE
+        binding.puanTextView.visibility = View.GONE
         number = 0.0
-
         runnable = object : Runnable {
             override fun run() {
-
                 number = number + 1
                 binding.timerText.text = (number).toString()
-
                 handler.postDelayed(this,10)
-
+                if(number>(300-(level))){timeUp()}
             }
-
         }
-
         handler.post(runnable)
         binding.startButton.isEnabled = false
     }
 
-    fun stop() {
-        handler.removeCallbacks(runnable)
-
-    }
+    fun stop() { handler.removeCallbacks(runnable) }
 
     fun catchedCircle (view: View){
         Toast.makeText(this, "Tebrikler", Toast.LENGTH_SHORT).show()
-        /*val alertDialogBuilder = AlertDialog.Builder(this@MainActivity)
-        alertDialogBuilder.setTitle("Result")
-        alertDialogBuilder.setMessage("Tebrikler")
-        alertDialogBuilder.show()*/
+        println("Correct")
         stop()
         if(bestScore == null){
-            bestScore=(number/100)
+            bestScore = (number/100)
         }else{
-            if(bestScore!! > (number/100)){bestScore=(number/100)}
+            bestScore = ((bestScore!! * (level-1)) +  (number/100))/level
         }
+        puan = level*(1/bestScore!!)
+        binding.puanTextView.text = "Puan: ${puan.toString()}"
+        println(puan)
+        level += 1
         binding.scoreTextView.text = "Score: ${bestScore}"
-        binding.startButton.isEnabled = true
         binding.timerText.text = "Time: ${number/100}"
+        binding.levelTextView.text = "Level: ${level.toString()}"
+        startAction(view)
 
     }
     fun betouched (view: View){
-        Toast.makeText(this, "Yanlış Cevap", Toast.LENGTH_SHORT).show()
-        /*if(imageColor = myColors[randomNumber]!!.get(0).toString().toInt()){
-            Toast.makeText(this, "Tebrikler", Toast.LENGTH_SHORT).show()
-        } else{
-            Toast.makeText(this, "Yanlış Cevap", Toast.LENGTH_SHORT).show()
-        }*/
+        resetGame("Yanlış Cevap")
+    }
+
+    fun timeUp(){
+        resetGame("Süre Doldu!")
+    }
+
+    fun resetGame(message:String){
+        for (iv in binding.circleLinearLayout.children){
+            var imageView = iv as ImageView
+            imageView.isEnabled=false
+        }
+        val alertDialogBuilder = AlertDialog.Builder(this@MainActivity)
+            .setTitle("Result")
+            .setMessage(message)
+            .show()
         stop()
+        binding.startButton.isEnabled = true
+        println("Try Again")
+        binding.puanTextView.visibility = View.VISIBLE
+        bestScore = null
+        number = 0.0
+        level = 1
+        puan=0.0
 
     }
 }
